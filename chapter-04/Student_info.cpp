6d0ebff903272af5427b0961c93f114067f2a9c7
Chi <chiyc@live.com>
2016-06-23 08:13:30 -0600

    Add partitioned program for chapter-04

--- chapter-04/Student_info.cpp
+++ chapter-04/Student_info.cpp
@@ -2,13 +2,13 @@
 
 using std::istream; using std::vector;
 
-bool compare(Student_info& x, Student_info& y){
+bool compare(const Student_info& x, const Student_info& y){
     return x.name < y.name;
 }
 
 istream& read(istream& is, Student_info& s){
     is >> s.name >> s.midterm >> s.final;
-    read_hw(istream& is, s.homework);
+    read_hw(is, s.homework);
 
     return is;
 }
